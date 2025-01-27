d536af8f5fa2ee2102febc558f6db1b7c926c5a4
Abel  Vilca <abel.vilca.1212@gmail.com>
2025-01-26 20:01:52 -0500

    Update app.js
    funciones y validaciones de insercion

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = []
+let amigos = [];
+
+function agregarAmigo(){
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+
+    // Validar que el campo no esté vacío
+    if(nombreAmigo === ''){
+        alert('Debes ingresar un nombre');
+        return;
+    }
+
+    // Validar que el nombre no se repita
+    if(amigos.includes(nombreAmigo)){
+        alert('El amigo ya fue agregado');
+        return;
+    }
+
+    //Agregar el nombre al array de amigos
+    amigos.push(nombreAmigo);
+
+    //Limpiar el campo de entrada
+    inputAmigo.ariaValu = "";
+
+    //Actualizar la lista en el HTML
+    actualizarLista();
+
+}
 
 
 
